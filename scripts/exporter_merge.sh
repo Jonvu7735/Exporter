47563e2326f3fccc551e1c14178d5f4d310c6249
Jonvu7735 <thanhtung.itpr@gmail.com>
2019-07-05 17:17:39 +0700

    Update exporter_merge.sh

--- scripts/exporter_merge.sh
+++ scripts/exporter_merge.sh
@@ -33,8 +33,7 @@ function init_file() {
 		sudo chown -R $USER:$USER /etc/init.d/exporter_*
 		sudo chkconfig --add ${exp_name} >/dev/null 2>&1 
 		sudo chkconfig on ${exp_name} >/dev/null 2>&1 
-		echo \n "Init File: "
-		echo -e $done
+		echo -e "Init File: $done "
 
 	elif [[ $os == 7 ]]; then
 		yes | sudo cp -f $SVPATH/${exp_name}/systemd/${exp_name}.service /etc/systemd/system/
@@ -43,48 +42,46 @@ function init_file() {
 		sudo chown -R $USER:$USER /etc/systemd/system/exporter_*.service
 		sudo systemctl daemon-reload >/dev/null 2>&1 
 		sudo systemctl enable ${exp_name}.services >/dev/null 2>&1 
-		echo \n "Init File: "
-		echo -e $done	
+		echo -e "Init File: $done " 
+	
 
     else
-       echo \n "Can not detect OS"
+       echo -e "Can not detect OS"
     fi
 }
 function stop_exporter() {
 	os=`cat /etc/redhat-release | grep -oP '(?<= )[0-9]+(?=\.)'`
 	local pid=`ps aux | grep -v grep | grep "${exp_name}" | sed 's/  \+/ /g' | cut -d' ' -f2`
 	if [[ $os == 6 ]]; then
-		sudo kill -9 $pid
-		echo \n $"Stopping $exp_name: "
-		echo -e $success
+		sudo kill -9 $pid >/dev/null 2>&1
+		echo -e "Stopping $exp_name: $success" 
 	elif [[ $os == 7 ]]; then
-		sudo kill -9 $pid
-		echo \n $"Stopping $exp_name: "
-		echo -e $success
+		sudo kill -9 $pid >/dev/null 2>&1
+		sudo systemctl stop ${exp_name}.service 
+		echo -e "Stopping $exp_name: $success"
 	else
-        echo \n "Process ${exp_name} NOT RUN"
+        echo -e "Process ${exp_name} NOT RUN"
 	fi
 }
 function start_exporter() {
 	os=`cat /etc/redhat-release | grep -oP '(?<= )[0-9]+(?=\.)'`
 	if [[ $os == 6 ]]; then
-		/etc/init.d/${exp_name} start
-		echo \n $"Start $exp_name: "
-		echo -e $success
+		/etc/init.d/${exp_name} start 
+		echo -e $"Start $exp_name: $success"
 	elif [[ $os == 7 ]]; then
-		sudo systemctl start ${exp_name}.service
-		echo \n $"Start $exp_name: "
-		echo -e $success
+		sudo systemctl start ${exp_name}.service 
+		echo -e $"Start $exp_name: $success"
 	else
-        echo \n "Can not start ${exp_name}"
+        echo "Can not start ${exp_name}"
 	fi
 }
 
 # Step 1
-stop_exporter 
+stop_exporter >> ${DLog}
 # Step 2
 check_log
 init_file
 # Step 3
-start_exporter
+start_exporter >> ${DLog}
+echo -n "Install ${exp_name} Complete"
 # END
