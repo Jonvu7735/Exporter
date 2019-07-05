adfc2098ee4404864e65a48128864708774d4e67
Jonvu7735 <thanhtung.itpr@gmail.com>
2019-07-05 13:15:01 +0700

    Update

--- main.sh
+++ main.sh
@@ -8,6 +8,8 @@ success="[$Gre OK $RCol]"
 fail="[$Red Fail $RCol]"
 ########################################################################################################################################################
 # Declare Variables
+exp_name="exprter_merge"
+# Not need change
 DTIME=$(date +"%Y%m%d")
 SDIR=`pwd`
 HOMEPATH="/etc/prometheus"
@@ -16,13 +18,12 @@ BINARYPATH=${HOMEPATH}/sbin
 LOGPATH=${HOMEPATH}/logs
 CNFPATH=${HOMEPATH}/var
 SVPATH=${HOMEPATH}/services
+SCRPATH=${HOMEPATH}/scripts
 DLog="${LOGPATH}/deploy_$IP_$DTIME.log"
-merge_port="11011"
-node_port="11020"
 ########################################################################################################################################################
 # Check WorkDIR
 function check_homepath() {
-	arr_path=(${HOMEPATH} ${BINARYPATH} ${LOGPATH} ${CNFPATH} ${SVPATH})
+	arr_path=(${HOMEPATH} ${BINARYPATH} ${LOGPATH} ${CNFPATH} ${SVPATH} ${SCRPATH})
 	for hpath in "${arr_path[@]}"
 	do 
 		[ ! -d "${hpath}" ] && sudo mkdir -p "${hpath}"
@@ -40,79 +41,74 @@ function check_user() {
 }
 # Log File
 function check_log() {
-	[ ! -f "$LOGPATH/exporter_merge_${DTIME}.log" ] && touch $LOGPATH/exporter_merge_${DTIME}.log
-	[ ! -f "$LOGPATH/exporter_node_${DTIME}.log" ] && touch $LOGPATH/exporter_node_${DTIME}.log
-	sudo chown -R $USER $LOGPATH 
+	[ ! -f "$LOGPATH/${exp_name}_${DTIME}.log" ] && touch $LOGPATH/${exp_name}_${DTIME}.log
+	sudo chown -R $USER:$USER $HOMEPATH 
 }
 # Update 
 function update_source() {
-	sudo cp -R $SDIR/sbin $HOMEPATH
-	sudo cp -R $SDIR/services $HOMEPATH
-	sudo cp $SDIR/var/exporter_merge.yaml $CNFPATH
+	yes | sudo cp -rf $SDIR/sbin $HOMEPATH
+	yes | sudo cp -rf $SDIR/services $HOMEPATH
+	yes | sudo cp -rf $SDIR/scripts $HOMEPATH
+	yes | sudo cp $SDIR/var/${exp_name}.yaml $CNFPATH
 	sudo chmod +x $BINARYPATH -R
-}
-# Funtion Start/Stop/Restart
-function stop_exporter() {
-	ps=`ps aux | grep -v grep | grep -v rsync | grep "${prog}"`
-	c=`ps aux | grep -v grep | grep -v rsync | grep "${prog}" | wc -l`
-
-	echo -n $"Stopping $prog: "
-	if [ $c -gt 0 ]; then
-		pids=`echo "$ps" | sed 's/  \+/ /g' | cut -d' ' -f2`
-		sudo kill -9 $pids 
-		echo -e $success
-	fi
-}
-function start_exporter() {
-	#ps=`ps aux | grep -v grep | grep -v rsync | grep "${prog}" | awk 'BEGIN{FS="/exporter_"}{print $2}' | awk '{print $1}'`
-    ps="${prog}"
-	   if [[ $ps == exporter_merge ]]; then
-		bash -c "${BINARYPATH}/${prog} -c ${CNFPATH}/${prog}.yaml --listen-port $merge_port >> $LOGPATH/exporter_merge_$DTIME.log 2>&1 &"
-		echo -e $success.
-	   elif [[ $ps == exporter_node ]]; then
-		bash -c "${BINARYPATH}/${prog} --web.listen-address=:${node_port} >> $LOGPATH/exporter_node_$DTIME.log 2>&1 &"
-		echo -e $success
-	fi
 }
 function init_file() {
         os=`cat /etc/redhat-release | grep -oP '(?<= )[0-9]+(?=\.)'`
 	if [[ $os == 6 ]]; then
-		[ ! -f "/etc/init.d/exporter_merge" ] && sudo cp $SVPATH/exporter_merge/init.d/exporter_merge /etc/init.d/
-		[ ! -f "/etc/init.d/exporter_node" ] && sudo cp $SVPATH/exporter_node/init.d/exporter_node /etc/init.d/
+		[ ! -f "/etc/init.d/${exp_name}" ] && sudo cp $SVPATH/${exp_name}/init.d/${exp_name} /etc/init.d/
+
 		sudo chmod +x /etc/init.d/exporter_*
-		sudo chkconfig --add exporter_merge >/dev/null 2>&1 
-		sudo chkconfig on exporter_merge >/dev/null 2>&1 
-		sudo chkconfig --add exporter_node >/dev/null 2>&1
-		sudo chkconfig on exporter_node >/dev/null 2>&1
+		sudo chown $USER:$USER /etc/init.d/exporter_*
+		sudo chkconfig --add ${exp_name} >/dev/null 2>&1 
+		sudo chkconfig on ${exp_name} >/dev/null 2>&1 
+
 	elif [[ $os == 7 ]]; then
-		[ ! -f "/etc/systemd/system/exporter_merge.service" ] && sudo cp $SVPATH/exporter_merge/systemd/exporter_merge.service /etc/systemd/system/
-		[ ! -f "/etc/systemd/system/exporter_node.service" ] && sudo cp $SVPATH/exporter_node/systemd/exporter_node.service /etc/systemd/system/
+		[ ! -f "/etc/systemd/system/${exp_name}.service" ] && sudo cp $SVPATH/${exp_name}/systemd/${exp_name}.service /etc/systemd/system/
+
 		sudo chmod +x /etc/systemd/system/exporter_*.service
+		sudo chown $USER:$USER /etc/systemd/system/exporter_*.service
 		sudo systemctl daemon-reload
-		sudo systemctl enable exporter_merge.service
-		sudo systemctl enable exporter_node.service
+		sudo systemctl enable ${exp_name}.service
+
     else
        echo "Can not detect OS"
     fi
 }
+# Funtion Start/Stop
+function stop_exporter() {
+	os=`cat /etc/redhat-release | grep -oP '(?<= )[0-9]+(?=\.)'`
+	if [[ $os == 6 ]]; then
+		/etc/init.d/${exp_name} stop
+		echo \n $"Stopping $exp_name: "
+	elif [[ $os == 7 ]]; then
+		sudo systemctl stop ${exp_name}.service
+		echo \n $"Stopping $exp_name: "
+	else
+        echo \n "Can not stop ${exp_name}"
+	fi
+}
+function start_exporter() {
+	os=`cat /etc/redhat-release | grep -oP '(?<= )[0-9]+(?=\.)'`
+	if [[ $os == 6 ]]; then
+		/etc/init.d/${exp_name} start
+		echo \n $"Start $exp_name: "
+	elif [[ $os == 7 ]]; then
+		sudo systemctl start ${exp_name}.service
+		echo \n $"Start $exp_name: "
+	else
+        echo \n "Can not start ${exp_name}"
+	fi
+}
+
 # Step 1: Stop service
-arr_path_1=("exporter_merge" "exporter_node")
-for prog in "${arr_path_1[@]}"
-    do
-         stop_exporter
-    done
-echo -n ""
+stop_exporter
 # Step 2: Base Check
 check_homepath
 check_user
 check_log
 update_source
 init_file
-# Step 2: Start Exporter_Merge & Node
-arr_path_2=("exporter_merge" "exporter_node")
-for prog in "${arr_path_2[@]}"
-	do 
-		start_exporter
-	done
+# Step 2: Start ${exp_name} & Node
+start_exporter
 echo "Install successful"
 #END
---
+++ scripts/exporter_node.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# Declare Variables
+### Change here
+exp_name="exporter_node"
+### Not need change
+DTIME=$(date +"%Y%m%d")
+HOMEPATH="/etc/prometheus"
+USER='prometheus'
+BINARYPATH=${HOMEPATH}/sbin
+LOGPATH=${HOMEPATH}/logs
+CNFPATH=${HOMEPATH}/var
+SVPATH=${HOMEPATH}/services
+DLog="${LOGPATH}/deploy_$IP_$DTIME.log"
+
+### FUNTION 
+function check_log() {
+	[ ! -f "$LOGPATH/${exp_name}_${DTIME}.log" ] && touch $LOGPATH/${exp_name}_${DTIME}.log
+	sudo chown -R $USER:$USER $HOMEPATH
+}
+function init_file() {
+    os=`cat /etc/redhat-release | grep -oP '(?<= )[0-9]+(?=\.)'`
+	if [[ $os == 6 ]]; then
+		[ ! -f "/etc/init.d/${exp_name}" ] && sudo cp $SVPATH/${exp_name}/init.d/${exp_name} /etc/init.d/
+
+		sudo chmod +x /etc/init.d/exporter_*
+		sudo chown -R $USER:$USER /etc/init.d/exporter_*
+		sudo chkconfig --add ${exp_name} >/dev/null 2>&1 
+		sudo chkconfig on ${exp_name} >/dev/null 2>&1 
+
+	elif [[ $os == 7 ]]; then
+		[ ! -f "/etc/systemd/system/${exp_name}.service" ] && sudo cp $SVPATH/${exp_name}/systemd/${exp_name}.service /etc/systemd/system/
+
+		sudo chmod +x /etc/systemd/system/exporter_*.service
+		sudo chown -R $USER:$USER /etc/systemd/system/exporter_*.service
+		sudo systemctl daemon-reload >/dev/null 2>&1 
+		sudo systemctl enable ${exp_name}.services >/dev/null 2>&1 	
+
+    else
+       echo \n "Can not detect OS"
+    fi
+}
+function stop_exporter() {
+	os=`cat /etc/redhat-release | grep -oP '(?<= )[0-9]+(?=\.)'`
+	if [[ $os == 6 ]]; then
+		/etc/init.d/${exp_name} stop
+		echo \n $"Stopping $exp_name: "
+	elif [[ $os == 7 ]]; then
+		sudo systemctl stop ${exp_name}.service
+		echo \n $"Stopping $exp_name: "
+	else
+        echo \n "Can not stop ${exp_name}"
+	fi
+}
+function start_exporter() {
+	os=`cat /etc/redhat-release | grep -oP '(?<= )[0-9]+(?=\.)'`
+	if [[ $os == 6 ]]; then
+		/etc/init.d/${exp_name} start
+		echo \n $"Start $exp_name: "
+	elif [[ $os == 7 ]]; then
+		sudo systemctl start ${exp_name}.service
+		echo \n $"Start $exp_name: "
+	else
+        echo \n "Can not start ${exp_name}"
+	fi
+}
+
+# Step 1
+stop_exporter >> $DLog
+# Step 2
+check_log >> $DLog
+init_file >> $DLog
+# Step 3
+start_exporter >> $DLog
+# END
